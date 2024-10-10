a4311e2d3db5626ddb6cbde0538c94cf87363e8b
jackt24 <48061691+jackt24@users.noreply.github.com>
2024-10-10 09:29:49 +0100

    troubleshoot cucumber

--- cucumber-runner.js
+++ cucumber-runner.js
@@ -1,12 +1,11 @@
-// cucumber-runner.js
 const { setWorldConstructor, setDefaultTimeout } = require('@cucumber/cucumber');
-const playwright = require('@playwright/test');
+const { chromium } = require('playwright');
 
 setDefaultTimeout(60 * 1000);
 
 class CustomWorld {
   async launchBrowser() {
-    this.browser = await playwright.chromium.launch({headless:true});
+    this.browser = await chromium.launch({ headless: true }); // Make sure headless is set to true
     this.context = await this.browser.newContext();
     this.page = await this.context.newPage();
   }
